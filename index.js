efc563d7ade775d946479da2655e6595fe64cc0c
chicojnr <souza.jnr@gmail.com>
2023-08-19 16:37:52 -0400

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function formatarDataHora(data) {
+    const dia = String(data.getDate()).padStart(2, '0');
+    const mes = String(data.getMonth() + 1).padStart(2, '0'); // Os meses são indexados a partir de 0
+    const ano = data.getFullYear();
+    const horas = String(data.getHours()).padStart(2, '0');
+    const minutos = String(data.getMinutes()).padStart(2, '0');
+    const segundos = String(data.getSeconds()).padStart(2, '0');
+    return `${dia}/${mes}/${ano} ${horas}:${minutos}:${segundos}`;
+}
+
+wss.on('connection', (ws) => {
+    console.log('Cliente conectado');
+
+    ws.on('close', () => {
+        console.log('Cliente desconectado');
+    });
+});
+
+app.get('/send-data', (req, res) => {
+    const equipId = req.query.equipid;
+    const temperatura = req.query.temp;
+    const estadoPorta = req.query.door;
+
+    const data = {
+        equipId,
+        temperatura,
+        estadoPorta,
+        timestamp: formatarDataHora(new Date())
+    };
+
+    wss.clients.forEach((client) => {
+        client.send(JSON.stringify(data));
+    });
+    console.log(req.query)
+    res.status(200).send();
+});
+
+server.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
