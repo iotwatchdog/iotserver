7830a62430b68de716df38ee91db23b71ea3a1e3
chicojnr <souza.jnr@gmail.com>
2023-08-22 01:01:17 -0400

    Fix

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const app = express();
+app.use(express.json());
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-// app.use(express.json);
 
 
 function formatarDataHora(data) {
@@ -44,23 +44,22 @@ app.get('/send-data', (req, res) => {
     res.status(200).send();
 });
 
-// app.post('/post-data', (req, res) => {
-//     console.log(req.body)
-//     const { equipId, temp, door } = req.body;
-//     const data = {
-//         equipId,
-//         temp,
-//         door,
-//         timestamp: formatarDataHora(new Date())
-//     };
+app.post('/post-data', (req, res) => {
+    console.log(req.body)
+    const { equipId, temp, door } = req.body;
+    const data = {
+        equipId,
+        temp,
+        door,
+        timestamp: formatarDataHora(new Date())
+    };
 
-//     wss.clients.forEach((client) => {
-//         client.send(JSON.stringify(data));
-//     });
+    wss.clients.forEach((client) => {
+        client.send(JSON.stringify(data));
+    });
 
-//     console.log(req.body);
-//     res.status(200).send();
-// });
+    res.status(200).send();
+});
 
 server.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
